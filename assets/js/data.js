bff62d919454bb48af068068c587019be115c5b1
M-Narvaez <mnarvaezmendoza49@correo.unicordoba.edu.co>
2022-07-25 21:53:31 -0500

    Nuevos ajustes

--- assets/js/data.js
+++ assets/js/data.js
@@ -2,7 +2,8 @@ const API_URL = "http://localhost:9000";
 
 const juegoDestacado = document.querySelector("#juegoDestacado");
 const imagenDestacada = document.querySelector('#imagenDestacada');
-const juegosDestacados = document.querySelector('#videojuegosDestacados')
+const juegosDestacados = document.querySelector('#videojuegosDestacados');
+const listaVideojuegos = document.querySelector('#videojuegos')
 
 fetch(`${API_URL}/videojuego/1202`)
     .then((response) => response.json())
@@ -15,20 +16,43 @@ fetch(`${API_URL}/videojuego/1202`)
                         
                         const imagenD = videojuegos.map((juego) => `<img src="${juego.imagen}" alt="" class="home__img">`);
 
-                        const callOfDuty = videojuegos.map((juego) => `<article class="featured__card">
-                                                                        <span class="featured__tag">Venta</span>
-
-                                                                        <img src="${juego.imagen}" alt="" class="featured__img">
-
-                                                                        <div class="featured__data">
-                                                                            <h3 class="featured__title">${juego.nombreVideojuego}</h3>
-                                                                            <span class="featured__price">$${juego.Valor}</span>
-                                                                        </div>
-
-                                                                        <button class="button featured__button">AÑADIR AL CARRITO</button>
-                                                                        </article>`);
-
         juegoDestacado.innerHTML = juegoD;
         imagenDestacada.innerHTML = imagenD;
-        juegosDestacados.innerHTML = callOfDuty;
+    });
+
+fetch(`${API_URL}/videojuego/destacados`)
+    .then((response) => response.json())
+    .then((videojuegos) => {
+        const jDestacados = videojuegos.map((juego) => `<article class="featured__card">
+        <span class="featured__tag">Venta</span>
+
+        <img src="${juego.imagen}" alt="" class="featured__img">
+
+        <div class="featured__data">
+            <h3 class="featured__title">${juego.nombreVideojuego}</h3>
+            <span class="featured__price">$${juego.Valor}</span>
+        </div>
+
+        <button class="button featured__button">AÑADIR AL CARRITO</button>
+        </article>`);
+
+        juegosDestacados.innerHTML = jDestacados;
+    });
+
+
+fetch(`${API_URL}/videojuego`)
+    .then((response) => response.json())
+    .then((juegos) => {
+        const allGames = juegos.map((juego) => `<article class="products__card">
+        <img src="/${juego.imagen}" alt="" class="products__img">
+
+        <h3 class="products__title">${juego.nombreVideojuego}</h3>
+        <span class="products__price">$${juego.Valor}</span>
+
+        <button class="products__button">
+            <i class='bx bx-shopping-bag'></i>
+        </button>
+        </article>`);
+
+        listaVideojuegos.innerHTML = allGames;
     });
